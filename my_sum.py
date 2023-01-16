fafc1321c7c8f00c117ae846ac2e75d1aac41536
AntonLeonardovichMarchenko <65681798+AntonLeonardovichMarchenko@users.noreply.github.com>
2023-01-16 22:57:35 +0300

    Create my_sum.py

---
+++ my_sum.py
@@ -0,0 +1,12 @@
+
+def sum(arg):
+
+    print(f'++++++++++ this is sum! ++++++++++')
+
+    total = 0
+    for val in arg:
+        total += val
+
+    print(f'+++++ the result of sum is {total} +++++')
+
+    return total
